0c40f44bb73bb7400a5f346aeaf7032e40ae4678
NatanaelTerreno <73486181+njterreno@users.noreply.github.com>
2021-06-29 22:02:43 -0300

    Add files via upload

---
+++ Datos/ModelBDBiblioSystem.Designer.cs
@@ -0,0 +1,4 @@
+﻿// La generación de código predeterminada está deshabilitada para el modelo 'C:\Users\NAJOTE\Desktop\TP FINAL PRACTICA\Biblioteca - Terreno, Natanael\Datos\ModelBDBiblioSystem.edmx'. 
+// Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
+// por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
+// el modelo en el diseñador.
